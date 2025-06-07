da59221ac64612e460323cb4a9d4b49355481538
binh1922004 <binh1920042018@gmail.com>
2025-06-07 16:41:56 +0700

    resolve conflict

--- src/main/java/com/accessed/miniproject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/accessed/miniproject/exception/GlobalExceptionHandler.java
@@ -1,45 +1,45 @@
-package com.accessed.miniproject.exception;
-
-import com.accessed.miniproject.enums.EErrorCode;
-import com.accessed.miniproject.dto.response.ApiResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.nio.file.AccessDeniedException;
-
-@ControllerAdvice
-public class GlobalExceptionHandler {
-
-
-    @ExceptionHandler(value = Exception.class)
-    ResponseEntity<ApiResponse<String>> exceptionHandler(Exception e) {
-        ApiResponse<String> apiResponse = new ApiResponse<>();
-        apiResponse.setCode(EErrorCode.UNCATEGORIZED.getCode());
-        apiResponse.setMessage(EErrorCode.UNCATEGORIZED.getMessage());
-        return ResponseEntity.badRequest().body(apiResponse);
-    }
-
-    @ExceptionHandler(value = AppException.class)
-    ResponseEntity<ApiResponse<String>> appExceptionHandler(AppException e) {
-        ApiResponse<String> apiResponse = new ApiResponse<>();
-        apiResponse.setCode(e.getErrorCode().getCode());
-        apiResponse.setMessage(e.getErrorCode().getMessage());
-        return ResponseEntity
-                .status(e.getErrorCode().getHttpStatusCode())
-                .body(apiResponse);
-    }
-
-    @ExceptionHandler(value = AccessDeniedException.class)
-    ResponseEntity<ApiResponse> accessDeniedExceptionHandler(AccessDeniedException e) {
-        EErrorCode errorCode = EErrorCode.FORBIDDEN;
-
-        return ResponseEntity.status(errorCode.getHttpStatusCode()).body(
-                ApiResponse.builder()
-                        .code(errorCode.getCode())
-                        .message(errorCode.getMessage())
-                        .build()
-        );
-    }
-
-}
+//package com.accessed.miniproject.exception;
+//
+//import com.accessed.miniproject.constant.ErrorCode;
+//import com.accessed.miniproject.dto.response.ApiResponse;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//
+//import java.nio.file.AccessDeniedException;
+//
+//@ControllerAdvice
+//public class GlobalExceptionHandler {
+//
+//
+//    @ExceptionHandler(value = Exception.class)
+//    ResponseEntity<ApiResponse<String>> exceptionHandler(Exception e) {
+//        ApiResponse<String> apiResponse = new ApiResponse<>();
+//        apiResponse.setCode(ErrorCode.UNCATEGORIZED.getCode());
+//        apiResponse.setMessage(ErrorCode.UNCATEGORIZED.getMessage());
+//        return ResponseEntity.badRequest().body(apiResponse);
+//    }
+//
+//    @ExceptionHandler(value = AppException.class)
+//    ResponseEntity<ApiResponse<String>> appExceptionHandler(AppException e) {
+//        ApiResponse<String> apiResponse = new ApiResponse<>();
+//        apiResponse.setCode(e.getErrorCode().getCode());
+//        apiResponse.setMessage(e.getErrorCode().getMessage());
+//        return ResponseEntity
+//                .status(e.getErrorCode().getHttpStatusCode())
+//                .body(apiResponse);
+//    }
+//
+//    @ExceptionHandler(value = AccessDeniedException.class)
+//    ResponseEntity<ApiResponse> accessDeniedExceptionHandler(AccessDeniedException e) {
+//        ErrorCode errorCode = ErrorCode.FORBIDDEN;
+//
+//        return ResponseEntity.status(errorCode.getHttpStatusCode()).body(
+//                ApiResponse.builder()
+//                        .code(errorCode.getCode())
+//                        .message(errorCode.getMessage())
+//                        .build()
+//        );
+//    }
+//
+//}
